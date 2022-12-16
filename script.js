ee33dc8f853e78abc159271a6e25a1f964f9a53b
Def <deferenseo@gmail.com>
2022-12-16 13:52:14 +0300

    added posibility start new game

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// Game flague
-let isPlaying = true;
-
 // Elements selection
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
@@ -10,12 +7,8 @@ const diceElement = document.querySelector('.dice');
 const player0Element = document.querySelector('.player--0');
 const player1Element = document.querySelector('.player--1');
 
-// Current score
-let currentScore = 0;
-let activePlayer = 0;
-
-// Total score
-const totalScores = [0, 0];
+// Current, Total score, active player, game flague
+let currentScore, activePlayer, totalScores, isPlaying;
 
 // Players current score
 const current0Element = document.querySelector('#current--0');
@@ -26,10 +19,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Game initial conditions
-score0Element.textContent = 0;
-score1Element.textContent = 0;
-diceElement.classList.add('hidden');
+// Default setting
+function setStartingState() {
+    score0Element.textContent = 0;
+    score1Element.textContent = 0;
+    current0Element.textContent = 0;
+    current1Element.textContent = 0;
+    player0Element.classList.remove('player--winner');
+    player1Element.classList.remove('player--winner');
+    player0Element.classList.remove('player--active');
+    player1Element.classList.remove('player--active');
+
+    if (!player0Element.classList.contains('player--active')) {
+        player0Element.classList.add('player--active');
+    }
+    diceElement.classList.add('hidden');
+    isPlaying = true;
+    currentScore = 0;
+    activePlayer = 0;
+    totalScores = [0, 0];
+}
+
+setStartingState();
 
 // Switch active player
 const switchActivePlayer = function() {
@@ -57,6 +68,7 @@ btnRoll.addEventListener('click', () => {
     }
 });
 
+
 btnHold.addEventListener('click', () => {
     if (isPlaying) {
         totalScores[activePlayer] += currentScore;
@@ -72,3 +84,5 @@ btnHold.addEventListener('click', () => {
         }
     }
 });
+
+btnNew.addEventListener('click', () => setStartingState());
